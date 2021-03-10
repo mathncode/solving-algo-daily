e15ba1cc0b677afe7e0b2e02e9196615d62bb735
mathncode <smilekhy@gmail.com>
2021-03-10 16:20:50 +0900

    3월 주간문제1 모든 테스트 통과!

--- weekly/2020-03-weekly1.py
+++ weekly/2020-03-weekly1.py
@@ -3,17 +3,19 @@
 def solution(s):
     ind_odd = []   # 홀수 길이 부분 문자열의 중심 인덱스
     ind_even = []   # 짝수 길이 부분 문자열의 중심 인덱스
-    sub_len_list = [0]  # 각 부분 문자열 최대 길이
+    sub_len_list = []  # 각 부분 문자열 최대 길이
     
     
-    if len(s) == 1:
+    if len(s) == 1:    # 1개 문자열
         ind_odd.append(0)    
     else:   # 부분 문자열 중심 인덱스 찾아 담기
         if s[0] == s[1]: ind_even.append(0)
         for i in range(1, len(s)-1):
             if s[i] == s[i+1]: ind_even.append(i)
             if s[i-1] == s[i+1]: ind_odd.append(i)
-        
+    if len(ind_odd) == 0: ind_odd.append(0)  # 나머지 2개이상 문자열(모든 글자가 다르거나, 팰린드롬이 없거나) 
+
+
     count = 0
     for i in range(len(ind_odd)):   # 홀수 길이 문자열 최대 길이 구하기
         for j in range(0, min(ind_odd[i], len(s) - ind_odd[i] - 1)):
@@ -33,4 +35,5 @@ def solution(s):
     answer = max(sub_len_list)  # 부분 문자열 중 최대 길이 구하기
     return answer
 
-# 테스트 케이스: "abcdcba", "abacde", "cdaade", "abcdcbajklml", "zxxzabcdcbajklmlcdaad", "a"    
+# 테스트 케이스: "abcdcba", "abacde", "cdaade", "abcdcbajklml", "zxxzabcdcbajklmlcdaad", "a", "abcde"
+# "a"와 "abcde", "lacl" 모두 결과가 1이 나와야 함.    
