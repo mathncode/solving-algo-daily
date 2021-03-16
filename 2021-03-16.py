90ceba96856cfd774a9939d6e7f1aad1195405a1
mathncode <smilekhy@gmail.com>
2021-03-16 09:30:02 +0900

    3월 16일 문제 해결, [내적]

---
+++ 2021-03-16.py
@@ -0,0 +1,9 @@
+# https://programmers.co.kr/learn/courses/30/lessons/70128
+# 내적
+
+def solution(a, b):
+    answer = 0
+    for i in range(len(a)):
+        answer += a[i] * b[i]
+    
+    return answer
