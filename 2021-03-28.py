31ba807c448f0118b434e84f52fe59eed5e6e383
mathncode <smilekhy@gmail.com>
2021-03-28 15:39:32 +0900

    3.28, 'x만큼 간격이 있는 n개 숫자'

--- 2021-03-28.py
+++ 2021-03-28.py
@@ -2,5 +2,5 @@
 # x만큼 간격이 있는 n개 숫자
 
 def solution(x, n):
-    # 등차수열의 일반항: 첫항 a, 공차 d 일때 a_n = a + (n-1)d
+    # 등차수열의 첫항 a, 공차 d일 때 일반항은 a_n = a + (n-1)d 이다.
     return [x + i * x for i in range(n)]
